2995cad8c250ad3f6d2ef0c57ac29fcaa54b6db4
mckib2 <nicholas.bgp@gmail.com>
2019-12-16 16:54:41 -0700

    Handle 2d+time fitting problems.

--- t1est/t1iter.py
+++ t1est/t1iter.py
@@ -4,22 +4,26 @@ import logging
 
 import numpy as np
 
-def t1iter(y, t, T10, A0=1, B0=-2, alpha=1, tol=1e-4, maxiter=100):
+def t1iter(
+        x, t, T10=1, A0=1, B0=-2, time_axis=-1, alpha=1, tol=1e-4,
+        maxiter=100):
     '''Try an iterative approach.
 
     Parameters
     ----------
-    y : array_like
+    x : array_like
         Measurements of the inversion recovery signal.
     t : array_like
         Inversion times (in sec).
-    T10, A0, B0 : floats
+    T10, A0, B0 : floats or array_like, optional
         Initial estimates for T1 (in sec), A, and B.
-    alpha : float
+    time_axis : int, optional
+        Axis that holds the time data.
+    alpha : float, optional
         Step-size for gradient descent updates to T1 estimate.
-    tol : float
+    tol : float, optional
         Break out of loop when abs(dT1) is below tol.
-    maxiter : int
+    maxiter : int, optional
         The maximum number of iterations to perform.
 
     Returns
@@ -30,32 +34,65 @@ def t1iter(y, t, T10, A0=1, B0=-2, alpha=1, tol=1e-4, maxiter=100):
         Number of iterations actually evaluated.
     err
         The derivative updates for T1 as a function of iteration.
+
+    Notes
+    -----
+    Uses a the following signal model:
+
+        s(t) = A + B exp(-t/T1)
+
+    Notice the sign convention of B is different than that used in
+    t1est.t1est().
     '''
+
+    assert x.ndim == 3, 'x should have xy+t dimensions!'
+
+    # Put time in the back
+    x = np.moveaxis(x, time_axis, -1)
+
+    # Do some sanity checks
+    if isinstance(T10, np.ndarray):
+        assert T10.shape == x.shape[:-1]
+    else:
+        T10 = np.ones(x.shape[:-1])*T10
+    if isinstance(A0, np.ndarray):
+        assert A0.shape == x.shape[:-1]
+    else:
+        A0 = np.ones(x.shape[:-1])*A0
+    if isinstance(B0, np.ndarray):
+        assert B0.shape == x.shape[:-1]
+    else:
+        B0 = np.ones(x.shape[:-1])*B0
 
     # Initialize estimates
     T1, A, B = T10, A0, B0
     N = len(t)
     ii = 0
-    dT1 = np.inf
 
     dT1s = np.zeros(maxiter)
-    while np.abs(dT1) > tol and ii < maxiter:
+    amaxdT1 = np.inf
+    while amaxdT1 > tol and ii < maxiter:
 
         # Least squares update for A, B
-        A = 1/N*np.sum(y - B*np.exp(-t/T1))
-        B = np.sum(np.exp(-t/T1)*(y - A))/np.sum(np.exp(-2*t/T1))
+        etT1 = np.exp(-t[None, None, :]/T1[..., None])
+        A = 1/N*np.sum(x - B[..., None]*etT1, axis=-1)
+        B = np.sum(etT1*(x - A[..., None]), axis=-1)/np.sum(
+            np.exp(-2*t[None, None, :]/T1[..., None]), axis=-1)
 
         # Do an update step for T1
         dT1 = np.sum(
-            -2*B*t*np.exp(-t/T1)*(y - A - B*np.exp(-t/T1)))/T1**2
-        dT1s[ii] = dT1
+            -2*B[..., None]*t[None, None, :]*etT1*(
+                x - A[..., None] - B[..., None]*etT1),
+            axis=-1)/T1**2
+        amaxdT1 = np.max(np.abs(dT1))
+        dT1s[ii] = amaxdT1
         T1 -= alpha*dT1
 
         ii += 1
 
     # Remove unused entries in dT1s
     if ii < maxiter:
-        dT1s = dT1s[:ii]
+        dT1s = dT1s[..., :ii]
     else:
         logging.warning(
             'Maximum number of iterations was reached! '
